69094e63ebb294aef885f61b9ed5dc6bdc3ed542
Aditya Shandilya <160865784+Programershanz@users.noreply.github.com>
2024-04-11 10:10:32 +0530

    Add files via upload

---
+++ multiplication_table.py
@@ -0,0 +1,10 @@
+#taking input
+num=int(input("enter a num"))
+
+#loop
+for i in range(1,11):
+
+
+#printing the table 
+     print ("{} x {} = {}".format(num,i,num*i))   
+    # print(num, 'X', i, '=', num*i)
